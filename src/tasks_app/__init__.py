0af167f195df2881b82b2d97dcf237d4619799d1
kmsayem12 <sayem312518@gmail.com>
2025-06-12 18:18:58 +0600

    add constants

---
+++ src/tasks_app/__init__.py
@@ -0,0 +1,3 @@
+from tasks_app.constants import STATUS_CHOICES, PRIORITY_CHOICES
+
+__all__ = ['STATUS_CHOICES', 'PRIORITY_CHOICES']
---
+++ src/tasks_app/constants.py
@@ -0,0 +1,12 @@
+STATUS_CHOICES = [
+    ('todo', 'To Do'),
+    ('in_progress', 'In Progress'),
+    ('done', 'Done'),
+    ('blocked', 'Blocked'),
+]
+
+PRIORITY_CHOICES = [
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High'),
+]
