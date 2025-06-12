93d94ff72cf75313376aac1573eedeccd8d8bc1c
kmsayem12 <sayem312518@gmail.com>
2025-06-12 12:59:28 +0600

    add task model

--- src/tasks_app/models.py
+++ src/tasks_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User # Using Django's built-in User model
 
 # Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ('todo', 'To Do'),
+        ('in_progress', 'In Progress'),
+        ('done', 'Done'),
+        ('blocked', 'Blocked'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default='medium')
+    due_date = models.DateField(null=True, blank=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='tasks_assigned')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='tasks_created')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ['due_date','priority']
+
+    def __str__(self):
+        return self.title
